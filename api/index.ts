dde2724cac6a43d8892292c35936bd9a4545462d
Revivalution <stacycobb@icloud.com>
2025-10-06 12:21:59 -0500

    Fix: Simplify serverless handler

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,6 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
 import serverlessHttp from 'serverless-http';
 import { app } from '../src/server.js';
 
-const handler = serverlessHttp(app);
-
-export default async function (req: VercelRequest, res: VercelResponse) {
-  // @ts-ignore serverless-http accepts (req,res)
-  return handler(req, res);
-}
+// Wrap Express app for Vercel serverless
+export default serverlessHttp(app);
 
